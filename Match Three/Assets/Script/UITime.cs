5688f78e0aafd28860c6d2981dba7a9b51fe8916
Muhammad Ramadhan <mramadhan742@gmail.com>
2021-09-26 16:47:02 +0700

    Added New Project
    Chapter 6 - Match Three

---
+++ Match Three/Assets/Script/UITime.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITime : MonoBehaviour
+{
+    public Text time;
+
+    private void Update()
+    {
+        time.text = GetTimeString(TimeManager.Instance.GetRemainingTime() + 1);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private string GetTimeString(float timeRemaining)
+    {
+        int minute = Mathf.FloorToInt(timeRemaining / 60);
+        int second = Mathf.FloorToInt(timeRemaining % 60);
+
+        return string.Format("{0} : {1}", minute.ToString(), second.ToString());
+    }
+}
